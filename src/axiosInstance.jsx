6344a07bd5d01faaada835779b5fb38ecc8b892c
Kaushalendra Mahato <kaushalendramahato87@gmail.com>
2026-07-23 22:39:59 +0545

    Fixed bug

--- src/axiosInstance.jsx
+++ src/axiosInstance.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_BACKEND_BASER_API || 'http://127.0.0.1:8000';
+const baseURL = import.meta.env.VITE_BACKEND_BASE_API || 'http://127.0.0.1:8000';
 
 const axiosInstance = axios.create({
   baseURL,
